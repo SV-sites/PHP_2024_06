d1522b35c1b8d7d11059291a6b90a51297e7830f
SV-sites <sonkauram@gmail.com>
2024-06-25 07:21:18 +0200

    Sum_up_comments

--- valtozok.php
+++ valtozok.php
@@ -1,4 +1,7 @@
 <?php
+
+// Something new with git!
+
 $name = "John";
 $age = 30;
 $is_student = true;
@@ -8,4 +11,25 @@ echo "Name: $name<br>";
 echo "Age: $age<br>";
 echo "Is Student: " . ($is_student ? 'Yes' : 'No') . "<br>";
 echo "Grades: " . implode(", ", $grades) . "<br>";
+
+
+// LETS some what i learned
+
+
+// implode() write the whole array in line and give separator
+
+// ($some_var ? "Yes" : "No") this is completly C
+
+// When you use some $var be cautius because you cant use '' singleones only duble ones ""
+
+
+
+
+
+// With the . i can connect the string line same es Javascript + sign
+
+// When array() i need to right it down array and use () instead of usual []
+
+// I think Best practice to use allways "" not ''
+
 ?>
